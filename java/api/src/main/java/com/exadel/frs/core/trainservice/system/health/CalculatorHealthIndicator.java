fa1813a317eff05e9eedae172008e5341a65e19a
jmalek <jmalek@exadel.com>
2025-10-01 12:51:53 +0200

    AIEAE-252: apply agent patch

---
+++ java/api/src/main/java/com/exadel/frs/core/trainservice/system/health/CalculatorHealthIndicator.java
@@ -0,0 +1,13 @@
+package com.exadel.frs.core.trainservice.system.health;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component("calculatorHealthIndicator")
+public class CalculatorHealthIndicator implements HealthIndicator {
+    @Override
+    public Health health() {
+        return Health.up().build();
+    }
+}
