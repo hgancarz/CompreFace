590039773d663543e6b0c133fc39949c2519e2af
example <example@example.com>
2025-10-23 12:37:10 +0000

    AIEAE-162: apply agent patch

---
+++ java/api/src/main/java/com/exadel/frs/core/trainservice/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.exadel.frs.core.trainservice.controller;
+
+import static com.exadel.frs.core.trainservice.system.global.Constants.API_V1;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(API_V1)
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    @ApiOperation(value = "Returns hello world message")
+    public String getHelloWorld() {
+        return "hello world";
+    }
+}
