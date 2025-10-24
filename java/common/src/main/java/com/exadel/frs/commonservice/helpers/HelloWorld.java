9644e3f5a10d6efebee31806b9a007ebca74922f
jmalek <jmalek@exadel.com>
2025-10-24 15:35:34 +0200

    AIEAE-162: apply agent patch

---
+++ java/common/src/main/java/com/exadel/frs/commonservice/helpers/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.exadel.frs.commonservice.helpers;
+
+/**
+ * Simple helper that returns a hello world string.
+ */
+public final class HelloWorld {
+
+    private HelloWorld() {
+        // utility class
+    }
+
+    public static String getHelloWorld() {
+        return "hello world";
+    }
+}
