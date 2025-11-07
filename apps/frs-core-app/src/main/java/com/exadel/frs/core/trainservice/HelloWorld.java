1f8bc5655bb80c8bc20caa8128deef48de262ff4
jmalek <jmalek@exadel.com>
2025-11-07 12:46:06 +0100

    AIEAE-162: apply agent patch

---
+++ apps/frs-core-app/src/main/java/com/exadel/frs/core/trainservice/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.exadel.frs.core.trainservice;
+
+public final class HelloWorld {
+
+    private HelloWorld() {
+        // utility class
+    }
+
+    public static String hello() {
+        return "hello world";
+    }
+
+    public static String helloWorld() {
+        return "hello world";
+    }
+
+    public String getGreeting() {
+        return "hello world";
+    }
+}
